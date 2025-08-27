77bec6890bb3ff33ae3339779730e3e09a379bda
Krzysztof <146448553+dymiasty@users.noreply.github.com>
2025-08-27 20:29:24 +0200

    Changed required EXILED version.

--- Plugin.cs
+++ Plugin.cs
@@ -17,7 +17,7 @@ namespace GejlonForExiledV2
         public static Plugin Instance { get; private set; }
         public override string Name => "GejlonForExiledV2";
         public override string Prefix => "GFEV2";
-        public override Version RequiredExiledVersion => new Version(9, 7, 2);
+        public override Version RequiredExiledVersion => new Version(9, 8, 1);
         public override string Author => "dymiasty";
         public override Version Version => new Version(0, 3, 4);
 
